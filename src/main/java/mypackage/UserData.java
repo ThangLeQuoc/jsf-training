f7aed060197a7ecdad525b63943105f59d9e0a7c
Thang Le Quoc <silverhair.guy@gmail.com>
2017-08-10 16:30:28 +0700

    show data

---
+++ src/main/java/mypackage/UserData.java
@@ -0,0 +1,81 @@
+package mypackage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "userData", eager = true)
+public class UserData {
+	private String name;
+	private String dept;
+	private int age;
+	private double salary;
+	private static final ArrayList<Employee> employees = new ArrayList<Employee>(Arrays.asList(
+			new Employee("John", "Marketing", 30, 2000.00), new Employee("Robert", "Marketing", 35, 3000.00),
+			new Employee("Mark", "Sales", 25, 2500.00), new Employee("Chris", "Marketing", 33, 2500.00),
+			new Employee("Peter", "Customer Care", 20, 1500.00)));
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public ArrayList<Employee> getEmployees() {
+		return employees;
+	}
+
+	public String saveEmployees() {
+
+		// set "canEdit" of all employees to false
+
+		for (Employee employee : employees) {
+			employee.setCanEdit(false);
+		}
+		return null;
+	}
+
+	public String addEmployee() {
+		Employee employee = new Employee(name, dept, age, salary);
+		employees.add(employee);
+		return null;
+	}
+
+	public String deleteEmployee(Employee employee) {
+		employees.remove(employee);
+		return null;
+	}
+
+	public String editEmployee(Employee employee) {
+		employee.setCanEdit(true);
+		return null;
+	}
+
+}
